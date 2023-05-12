be22ba98ef35cde29d7e6622fbd553e745497385
rexionmars <opensource.leonardi@gmail.com>
2023-05-12 15:09:29 -0300

    Add transition for contact page

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ContactPage = () => {
     return (
         <div>
-            <h1 className="text-center text-red-500 text-4xl">Em construção</h1>
+            <h1 className="trs text-center text-red-500 text-4xl">Em construção</h1>
         </div>
     )
 }
