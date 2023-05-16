0ca8ea36114f7da5191bd63522770f54248a10aa
rexionmars <opensource.leonardi@gmail.com>
2023-05-16 10:59:50 -0300

    Improved header navigator

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,19 +16,19 @@ const Header = () => {
                 <div className='w-full inline-block py-2'>
                     <div className='md:float-left block mt-2'>
                         <Link href='/'>
-                            <span className='cursor-pointer px-5 font-normal p-1 rounded-xl bg-sky-300 text-base text-zinc-700'>HOME</span>
+                            <span className='cursor-pointer px-5 font-normal p-1 rounded-xl transition-all duration-500 hover:bg-sky-400 bg-sky-300 text-base text-zinc-700'>HOME</span>
                         </Link>
                         <Link href='/about'>
-                            <span className='cursor-pointer px-5 m-4 font-normal p-1 rounded-xl bg-sky-300 text-base text-zinc-700'>Projeto</span>
+                            <span className='cursor-pointer px-5 m-4 font-normal p-1 rounded-xl transition-all duration-500 hover:bg-sky-400 bg-sky-300 text-base text-zinc-700'>Projeto</span>
                         </Link>
                         <Link href='/contact'>
-                            <span className='cursor-pointer px-5 font-normal p-1 rounded-xl bg-sky-300 text-base text-zinc-700'>Contatos</span>
+                            <span className='cursor-pointer px-5 font-normal p-1 rounded-xl transition-all duration-500 hover:bg-sky-400 bg-sky-300 text-base text-zinc-700'>Contatos</span>
                         </Link>
                     </div>
                     <div className='hidden md:float-left md:contents'>
                         {categories.map((category) => (
                             <Link key={category.slug} href={`/category/${category.slug}`}>
-                                <span className='md:float-right mt-2 align-middle text-zinc-700 ml-4 font-normal cursor-pointer bg-sky-200 px-2 py-1 rounded-lg'>
+                                <span className='md:float-right mt-2 align-middle ml-4 font-normal cursor-pointer transition-all duration-500 hover:bg-sky-300 hover:text-zinc-700 text-sky-300 border border-sky-300 px-2 py-1 rounded-xl'>
                                     {category.name}
                                 </span>
                             </Link>
