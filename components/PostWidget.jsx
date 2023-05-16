3a5c6265e03c0d80ee5a8ee43ca69d22b3008f01
rexionmars <opensource.leonardi@gmail.com>
2023-05-16 11:01:55 -0300

    Fixed image radius

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,36 +1,40 @@
 import React, { useEffect, useState} from 'react'
 import moment from 'moment'
 import Link from 'next/link'
+import Image from 'next/image';
 
+import { grpahCMSImageLoader } from '../util';
 import { getRecentPosts, getSimilarPosts } from '@/services'
 
 const PostWidget = ({ categories, slug }) => {
     const [relatedPosts, setRelatedPosts] = useState([]);
 
-    useEffect(() => {
-        if (slug) {
-            getSimilarPosts(categories, slug)
-                .then((result) => setRelatedPosts(result))
-        } else {
-            getRecentPosts()
-                .then((result) => setRelatedPosts(result))
-        }
-    }, [slug])
-
-    console.log(relatedPosts)
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) => {
+        setRelatedPosts(result);
+      });
+    } else {
+      getRecentPosts().then((result) => {
+        setRelatedPosts(result);
+      });
+    }
+  }, [slug]);
 
     return (
         <div className='post-shadow shadow-lg rounded-lg p-8 mb-8'>
-            <h3 className='text-xl mb-8 font-medium text-gray-300 border-b border-sky-900'>{slug ? 'Últimas postagens' : 'Postagens recentes'}</h3>
+            <h3 className='text-xl mb-8 font-medium text-gray-300 border-b border-gray-500'>{slug ? 'Últimas postagens' : 'Postagens recentes'}</h3>
             {relatedPosts.map((post) => (
                 <div key={post.title} className='flex items-center w-full mb-4'>
                     <div className='w-100 flex-none'>
-                        <img
-                            alt={post.title}
-                            height='60px'
-                            width='60px'
-                            className='align-middle rounded-full'
-                            src={post.featuredImage.url}
+                        <Image
+                          loader={grpahCMSImageLoader}
+                          alt={post.title}
+                          height={60}
+                          width={60}
+                          unoptimized
+                          className="align-middle rounded-md"
+                          src={post.featuredImage.url}
                         />
                     </div>
                     <div className='flex-grow ml-4'>
