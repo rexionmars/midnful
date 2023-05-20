15f991601f7ec2bd6a2618a2b3b4d24844398a0c
rexionmars <opensource.leonardi@gmail.com>
2023-05-20 10:56:42 -0300

    Improved text layout

--- components/Privacity.jsx
+++ components/Privacity.jsx
@@ -4,27 +4,45 @@ const PrivacityPage = () => {
     return (
         <div className="justify-center trs flex flex-row w-full align-middle mb-10">
             <div className="align-middle w-[80%]">
-                <h1 className="text-gray-200 text-4xl my-10">Termo de Uso do Site de Artigos e Dicas de Saúde Mental (Mindful Explorer)</h1>
+                <h1 className="text-gray-200 text-4xl my-5">Termo de Uso</h1>
                 <p className="text-gray-200 text-base">
-                    Bem-vindo(a) ao Mindful Explorer, nosso site de artigos e dicas de saúde mental! Antes de prosseguir, é importante que você leia e compreenda os termos e condições de uso descritos abaixo. Ao acessar e utilizar nosso site, você concorda em cumprir e estar sujeito a estes termos. Se você não concorda com algum aspecto deste documento, recomendamos que você não utilize nossos serviços.<br />
-                    1.Uso e Propósito do Site<br />
-                    1.1 O site tem como objetivo fornecer acesso a artigos escritos por psicólogos profissionais, contendo informações e dicas confiáveis sobre saúde mental, bem-estar e assuntos relacionados.<br />
+                    Bem-vindo(a) ao Mindful Explorer, nosso site de artigos e dicas de saúde mental! Antes de prosseguir, é importante que você leia e compreenda os termos e condições de uso descritos abaixo. Ao acessar e utilizar nosso site, você concorda em cumprir e estar sujeito a estes termos. Se você não concorda com algum aspecto deste documento, recomendamos que você não utilize nossos serviços.<br /><br />
+                    1.Uso e Propósito do Site
+                    <p className="text-left ml-10 my-5">
+                    1.1 O site tem como objetivo fornecer acesso a artigos escritos por psicólogos profissionais, contendo informações e dicas confiáveis sobre saúde mental, bem-estar e assuntos relacionados.<br /><br />
                     1.2 Os artigos disponíveis no site são fornecidos apenas para fins informativos e não substituem a consulta a um profissional de saúde mental. Eles não devem ser interpretados como aconselhamento médico ou psicológico, nem devem ser utilizados para realizar autodiagnósticos. Os criadores do site não se responsabilizam por quaisquer consequências decorrentes de autodiagnósticos feitos pelos usuários com base nos artigos publicados.<br />
-                    2.Proteção de Dados e LGPD<br />
-                    2.1 Reconhecemos a importância da privacidade e estamos comprometidos em proteger seus dados pessoais. Todo o processamento de dados é realizado em conformidade com a Lei Geral de Proteção de Dados (LGPD) e demais legislações aplicáveis.<br />
+                    </p>
+
+                    2.Proteção de Dados e LGPD
+                    <p className="text-left ml-10 my-5">
+                    2.1 Reconhecemos a importância da privacidade e estamos comprometidos em proteger seus dados pessoais. Todo o processamento de dados é realizado em conformidade com a Lei Geral de Proteção de Dados (LGPD) e demais legislações aplicáveis.<br /><br />
                     2.2 Ao utilizar nosso site, você concorda com a coleta, uso e divulgação de suas informações pessoais conforme descrito em nossa Política de Privacidade. Recomendamos que você leia atentamente nossa Política de Privacidade para entender como suas informações são tratadas.<br />
+                    </p>
+
                     3.Responsabilidade dos Criadores do Site<br />
-                    3.1 Os criadores do site não se responsabilizam por quaisquer consequências decorrentes do uso inadequado das redes sociais dos psicólogos divulgadas no projeto.<br />
-                    3.2 As redes sociais dos psicólogos divulgadas no site são de responsabilidade exclusiva dos respectivos profissionais. Os criadores do site não têm controle sobre o conteúdo publicado nessas redes sociais e não se responsabilizam por sua precisão, atualização, legalidade ou qualidade.<br />
-                    3.3 Os criadores do site não se responsabilizam por qualquer ação, conduta ou dano causado pelos psicólogos divulgados no projeto. É responsabilidade exclusiva do usuário verificar a credibilidade, qualificações e licenças dos profissionais de saúde mental antes de utilizar seus serviços.<br />
+
+                    <p className="text-left ml-10 my-5">
+                    3.1 Os criadores do site não se responsabilizam por quaisquer consequências decorrentes do uso inadequado das redes sociais dos psicólogos divulgadas no projeto.<br /><br />
+                    3.2 As redes sociais dos psicólogos divulgadas no site são de responsabilidade exclusiva dos respectivos profissionais. Os criadores do site não têm controle sobre o conteúdo publicado nessas redes sociais e não se responsabilizam por sua precisão, atualização, legalidade ou qualidade.<br /><br />
+                    3.3 Os criadores do site não se responsabilizam por qualquer ação, conduta ou dano causado pelos psicólogos divulgados no projeto. É responsabilidade exclusiva do usuário verificar a credibilidade, qualificações e licenças dos profissionais de saúde mental antes de utilizar seus serviços.<br /><br />
                     3.4 Os criadores do site não se responsabilizam por quaisquer consequências decorrentes do uso inadequado, comentários maldosos, ataques ou interações negativas por parte dos leitores ou usuários do site nas redes sociais dos psicólogos divulgadas no projeto.<br />
+                    </p>
+
                     4.Autorização de Uso de Dados<br />
-                    4.1 Os criadores do site estão autorizados a utilizar os dados fornecidos pelos psicólogos profissionais, com a devida autorização dos mesmos, apenas para fins de divulgação do artigo e do trabalho do psicólogo.<br />
+
+                    <p className="text-left ml-10 my-5">
+                    4.1 Os criadores do site estão autorizados a utilizar os dados fornecidos pelos psicólogos profissionais, com a devida autorização dos mesmos, apenas para fins de divulgação do artigo e do trabalho do psicólogo.<br /><br />
                     4.2 Os dados fornecidos pelos psicólogos serão tratados com confidencialidade e segurança, conforme descrito em nossa Política de Privacidade.<br />
+                    </p>
+
                     5.Alterações nos Termos de Uso<br />
-                    5.1 Os termos de uso podem ser atualizados periodicamente, e tais atualizações entrarão em vigor imediatamente após sua publicação no site.<br />
+
+                    <p className="text-left ml-10 my-5">
+                    5.1 Os termos de uso podem ser atualizados periodicamente, e tais atualizações entrarão em vigor imediatamente após sua publicação no site.<br /><br />
                     5.2 É responsabilidade do usuário revisar regularmente os termos de uso para estar ciente de quaisquer alterações.<br />
                     Ao utilizar nosso site, você concorda com estes termos de uso e se compromete a respeitar todas as disposições aqui mencionadas. Se você tiver alguma dúvida ou preocupação, entre em contato conosco através dos canais fornecidos no site.
+                    </p>
+
                     Atualizado em: 17/05/2023
                     </p>
                 <h1 className="text-gray-200 text-4xl my-10">Política de Privacidade</h1>
